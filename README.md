5d5d799a470025c615c17a9e748cffb9b3aa9b95
snamiki1212 <snamiki1212@gmail.com>
2021-07-14 14:58:56 -0700

    Update readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Overview
+
+https://user-images.githubusercontent.com/26793088/125698482-fad0e14f-ff06-40b0-b44b-0ac60d9d2ee3.mov
+
+## Technologies
+
+- Objective-C
+- Delegation Pattern
+
+## LICENSE
+
+MIT
--- payment/main.m
+++ payment/main.m
@@ -17,9 +17,8 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-        NSInteger randomNum = arc4random_uniform(900) + 100; // TODO: refactor getRandom(100, 1000)
+        NSInteger randomNum = arc4random_uniform(900) + 100 + 1; // TODO: refactor getRandom(100, 1000)
         NSString *msg = [[NSString alloc] initWithFormat: @"Thank you for shopping at Acme.com Your total today is $%d Please select your payment method:\n1: Paypal\n2: Stripe\n3: Amazon\n4: ApplePay\n", randomNum];
-        NSLog(@"%@", msg);
         
         // input
         InputHandler *inputHandler = [InputHandler new];
